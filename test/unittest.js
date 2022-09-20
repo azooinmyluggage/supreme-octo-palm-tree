3682de72e2abe86cd534c8b4603b0bb76d201264
Atul Malaviya <atul.malaviya@gmail.com>
2022-09-20 18:48:51 -0500

    Fixed test

--- test/unittest.js
+++ test/unittest.js
@@ -163,7 +163,7 @@ describe('add()', function () {
   const tests = [
     {args: [1, 2], expected: 3},
     {args: [1, 2, 3], expected: 6},
-    {args: [1, 2, 3, 4], expected: 11}
+    {args: [1, 2, 3, 4], expected: 10}
   ];
 
   tests.forEach(({args, expected}) => {
